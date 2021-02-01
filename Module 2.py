6b2cf69ab86e1a0f0109d093a0e0a2f1b21772d7
maelou363 <lindsay@maeloumedia.com>
2021-02-01 13:36:08 +0000

    Dictionaries and Pandas

--- Module 2.py
+++ Module 2.py
@@ -63,4 +63,79 @@ plt.xticks([1000, 10000, 100000],
 plt.show()
 
 
+# Dictionaries and Pandas
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Add italy to europe
+europe["italy"] = 'rome'
+
+# Print out italy in europe
+print('italy' in europe)
+
+# Add poland to europe
+europe["poland"] = 'warsaw'
+
+# Print europe
+print(europe)
+
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of germany
+europe["germany"] = 'berlin'
+
+# Remove australia
+del(europe["australia"])
+
+# Print europe
+print(europe)
+
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = {'capital': 'rome', 'population': 59.83}
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+
+# Print europe
+print(europe)
+
+
+# PANDAS
+
+# Pre-defined lists
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+# Import pandas as pd
+import pandas as pd
+
+# Create dictionary my_dict with three key:value pairs: my_dict
+my_dict = {'country': names, 'drives_right': dr, 'cars_per_cap': cpc}
+
+# Build a DataFrame cars from my_dict: cars
+cars = pd.DataFrame(my_dict)
+
+# Print cars
+print(cars)
+
+
+
+
 
