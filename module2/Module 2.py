48b56c74573f1c0021d4d0b20091c2555dd60763
Lindsay Garza <lmwressell@aol.com>
2021-02-01 15:09:54 +0000

    Completed Logic, Control Flow and Filtering

--- module2/Module 2.py
+++ module2/Module 2.py
@@ -136,6 +136,79 @@ cars = pd.DataFrame(my_dict)
 print(cars)
 
 
+#
+
+# Create arrays
+import numpy as np
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than 18.5 or smaller than 10
+print(np.logical_or(my_house > 18.5, my_house < 10))
+
+# Both my_house and your_house smaller than 11
+print(np.logical_and (my_house < 11, your_house < 11))
 
 
 
+# Define variables
+room = "kit"
+area = 14.0
+
+# if-else construct for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+else :
+    print("looking around elsewhere.")
+
+# if-else construct for area
+if area > 15 :
+    print("big place!")
+
+else :
+    print("pretty small.")
+
+
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Convert code to a one-liner
+sel = cars[cars['drives_right']]
+
+# Print sel
+print(sel)
+
+
+#Cars per capita (1)
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Create car_maniac: observations that have a cars_per_cap over 500
+cpc = cars['cars_per_cap']
+many_cars = cpc > 500
+car_maniac = cars[many_cars]
+
+# Print car_maniac
+print(car_maniac)
+
+
+#cars per capita (2) problem
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Import numpy, you'll need this
+import numpy as np
+
+# Create medium: observations with cars_per_cap between 100 and 500
+cpc = cars['cars_per_cap']
+between = np.logical_and(cpc > 100, cpc < 500)
+medium = cars[between]
+
+
+
+# Print medium
+print(medium)
