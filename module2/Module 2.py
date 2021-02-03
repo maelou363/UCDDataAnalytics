7c2b69b388a0bdc97bc2c966892dcb3e5c08cb64
Lindsay Garza <lmwressell@aol.com>
2021-02-03 15:30:09 +0000

    randint()

--- module2/Module 2.py
+++ module2/Module 2.py
@@ -302,3 +302,26 @@ for x in np_height :
 # For loop over np_baseball that visits every element of np_baseball
 for y in np.nditer(np_baseball) :
     print(y)
+
+
+
+
+# Numpy is imported, seed is set
+
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice != 6   :
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print (dice)
+print (step)
