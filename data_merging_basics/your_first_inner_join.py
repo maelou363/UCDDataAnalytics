429f0b36e955ecb564346a84bcb682de177c8537
Lindsay Garza <75320994+maelou363@users.noreply.github.com>
2021-02-25 06:59:12 +0000

    Update and rename text.txt to your_first_inner_join.py

---
+++ data_merging_basics/your_first_inner_join.py
@@ -0,0 +1,11 @@
+# You have been tasked with figuring out what the most popular types of fuel used in Chicago taxis are. To complete the analysis, you need to merge the taxi_owners and taxi_veh tables together on the vid column. You can then use the merged table along with the .value_counts() method to find the most common fuel_type.
+
+# Since you'll be working with pandas throughout the course, the package will be preloaded for you as pd in each exercise in this course. Also the taxi_owners and taxi_veh DataFrames are loaded for you.
+
+
+
+# Merge the taxi_owners and taxi_veh tables setting a suffix
+taxi_own_veh = taxi_owners.merge(taxi_veh, on='vid', suffixes=('_own','_veh'))
+
+# Print the value_counts to find the most popular fuel_type
+print(taxi_own_veh['fuel_type'].value_counts())
